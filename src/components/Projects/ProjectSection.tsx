d788c463d395ac522d75a70d797dc008f904908f
zazazawg <xdrifat9@gmail.com>
2025-10-18 23:19:26 +0600

    update: latest portfolio changes

--- src/components/Projects/ProjectSection.tsx
+++ src/components/Projects/ProjectSection.tsx
@@ -49,7 +49,6 @@ export default function ProjectSection() {
       });
     }, containerRef);
 
-    // Cleanup on unmount
     return () => {
       ctx.revert();
       requestAnimationFrame(() => ScrollTrigger.refresh());
